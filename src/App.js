b6fce2d6350b6880e883eb8b52a59ca252924b68
Chloe <chloe.jihye@gmail.com>
2019-10-24 15:01:21 +0700

    fetched URL and get issue title

--- src/App.js
+++ src/App.js
@@ -7,10 +7,16 @@ const clientId = process.env.REACT_APP_CLIENT_ID;
 console.log("id", clientId)
 function App() {
   const [token, setToken] = useState(null);
+  const [issues, setIssues] = useState([]);
+
+  const getIssues = async() => {
+    const response = await fetch (`https://api.github.com/repos/facebook/react/issues`)
+    const data = await response.json()
+    setIssues(data)
+    console.log ('issues', issues)
+  }
 
   useEffect(() => {
-
-
     const existingToken = sessionStorage.getItem("token");
     const accessToken =
       window.location.search.split("=")[0] === "?access_token"
@@ -24,18 +30,35 @@ function App() {
     }
 
     if (accessToken) {
-      console.log(`New accessToken: ${accessToken}`);
-
+      console.log(`New accessToken: ${accessToken}`); 
+      setToken(accessToken)
       sessionStorage.setItem("token", accessToken);
-
     }
 
     if (existingToken) {
       setToken(existingToken)
+      console.log ('token', token)
     }
-  });
+    getIssues()
+  }, []);
 
-  return <div className="App"></div>;
+  // const htmlIssues = issues.map(el => {
+  //   return <h1>{el.title}</h1>
+  // })
+
+  // console.log({htmlIssues})
+
+  return (
+    <div className = "App">
+    <div className = "IssueTitle">
+    <h3>Issue Title</h3> 
+      {issues.map(el => {
+        return  <li>{el.title}</li> 
+      })}
+
+    </div>
+    </div>
+  )
 }
 
 export default App;
