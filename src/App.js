c1e0a82f6acc604b00c6adf11ec7957633720af1
Nakasaky55 <themaskmen1996@gmail.com>
2019-10-25 13:22:25 +0700

    add pagination

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Repo from './component/Repo';
-import Nav from './component/Nav'
-import Footer from './component/Footer'
+import Repo from "./component/Repo";
+import Nav from "./component/Nav";
+import Footer from "./component/Footer";
 import { get } from "http";
+import Issues from "./component/Issues";
+import PaginationPack from "./component/Pagination";
 
 // require("dotenv").config({path: '.env'});
 const clientId = process.env.REACT_APP_CLIENT_ID;
-console.log("id", clientId);
+// console.log("id", clientId);
 function App() {
   const [token, setToken] = useState(null);
   const [issues, setIssues] = useState([]);
+  const [pageStatus, setPageStatus] = useState(null);
 
-  const getAPI = async () => {
-    
-    const url = `https://api.github.com/repos/facebook/react/issues?per_page=20`;
-    const response = await fetch(url);
+  const getAPI = async (
+    url = `https://api.github.com/repos/facebook/react/issues?access_token=73098386f1f5fbd159b090711b39ab2889842362&state=all`
+  ) => {
+    const headers = {
+      Accept: "application / vnd.github.v3 + json"
+    };
+
+    const response = await fetch(url, {
+      method: "GET",
+      headers: headers
+    });
 
     const jsonData = await response.json();
+    // const urls = response.headers.get("link").split(",").map(item=>item.split(";")[0].replace("<","").replace(">",""));
+    // console.log(urls)
 
-    console.table(jsonData);
+    const links = response.headers
+      .get("link")
+      .split(",")
+      .map(url => {
+        return {
+          link: url
+            .split(";")[0]
+            .replace("<", "")
+            .replace(">", ""),
+          value: url
+            .split(";")[1]
+            .trim()
+            .replace('"', "")
+            .replace('"', "")
+        };
+      });
+
+    setPageStatus(links);
+    console.log("links", links);
     setIssues(jsonData);
   };
   // console.log(issues);
@@ -52,7 +82,13 @@ function App() {
   return (
     <>
       <Nav />
-      <Repo issues={issues}/>
+      <Repo issues={issues} />
+      <PaginationPack
+        pageStatus={pageStatus && pageStatus}
+        setIssues={setIssues}
+        getAPI={getAPI}
+      />
+      {/* <Issues issues={issues} id={510496674} /> */}
       <Footer />
     </>
   );
--- src/component/Issues.js
+++ src/component/Issues.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import {Row, Container, Badge} from 'react-bootstrap'
 
-export default function Issues() {
+export default function Issues(props) {
+    const currentIssue = props.issues.filter ( ({id}) => id === props.id);
+    console.log("filter", currentIssue)
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <Badge variant="danger">{currentIssue[0] && currentIssue[0].state}</Badge>
+            <h1>{currentIssue[0] && currentIssue[0].title}</h1>
+            <p>{currentIssue[0] && currentIssue[0].body}</p>
+        </Container>
     )
 }
